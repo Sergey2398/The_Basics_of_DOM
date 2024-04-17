2b57c2774e42044a64f8cbcaaa05b5bb4fc66ab4
Sergey Travkin <s.travkin135@gmail.com>
2024-04-17 07:43:28 +0300

    Добавил данные для ДЗ 5

---
+++ 5/data.js
@@ -0,0 +1,39 @@
+const usersData = `
+[
+  {
+  "imageUrl": "https://w.forfun.com/fetch/07/07a5912517ee8e518442ba5631bf417f.jpeg",
+  "company": "Konopelski - Moen",
+  "username": "Owen.Leffler14",
+  "role": "administrator",
+  "phone": "(010) 945-1778"
+},
+{
+  "imageUrl": "https://img2.akspic.ru/attachments/originals/8/5/3/3/0/103358-priroda-pustynya-nebo-lug-rastitelnost-4321x2882.jpg",
+  "company": "Steuber - Senger",
+  "username": "Bonnie39",
+  "role": "guest",
+  "phone": "(518) 240-2855"
+},
+{
+  "imageUrl": "https://gas-kvas.com/grafic/uploads/posts/2023-10/1696602057_gas-kvas-com-p-kartinki-s-prirodoi-18.jpg",
+  "company": "Fritsch, Schinner and Prohaska",
+  "username": "Carlo92",
+  "role": "administrator",
+  "phone": "(097) 313-9120"
+},
+{
+  "imageUrl": "https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666389849_8-mykaleidoscope-ru-p-klassnaya-priroda-oboi-9.jpg",
+  "company": "Strosin Group",
+  "username": "Tess_Stracke40",
+  "role": "guest",
+  "phone": "(063) 392-0244"
+},
+{
+  "imageUrl": "https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666361504_9-mykaleidoscope-ru-p-peizazhi-prirodi-krasivo-9.jpg",
+  "company": "Hills - Legros",
+  "username": "Olaf.Emard",
+  "role": "user",
+  "phone": "(212) 202-4935"
+}
+]
+`
